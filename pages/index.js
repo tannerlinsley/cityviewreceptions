6eb03de4b696b4d376f5b5ce14943eaa0f2b2651
Zach-Allen <zach.allen2814@gmail.com>
2017-08-23 19:57:16 -0600

    Home page upgrades

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react'
-import styled from 'styled-components'
-import { injectGlobal } from 'styled-components'
-import reset from 'styled-reset'
+import React, { Component } from "react";
+import styled from "styled-components";
+import { injectGlobal } from "styled-components";
+import reset from "styled-reset";
 //
 
 injectGlobal`
@@ -10,10 +10,10 @@ injectGlobal`
     font-family: 'Lato', sans-serif;
     line-height: 1.3rem;
   }
-`
+`;
 
 const Header = styled.div`
-  height: 400px;
+  height: 1000px;
   width: 100%;
   background: url(/static/img/party.jpg);
   background-position: center;
@@ -41,27 +41,27 @@ const Header = styled.div`
     color: white;
     text-shadow: 0 0 2rem black, 0 0 2rem black;
   }
-`
+`;
 
 const Logo = styled.h1`
   width: 80%;
   position: relative;
   z-index: 1;
-  font-size: 1.8rem;
+  font-size: 4rem;
   font-weight: 300;
-  text-align: center;
+  text-align: left;
   color: white;
   padding: 1.5rem;
   text-shadow: 0 0 2rem black, 0 0 2rem black;
   border-bottom: 1px solid white;
   margin: 0 auto .5rem;
-`
+`;
 
 const Navbar = styled.div`
   position: relative;
   z-index: 1;
   color: white;
-  text-align: center;
+  text-align: right;
 
   li {
     display: inline-block;
@@ -80,22 +80,45 @@ const Navbar = styled.div`
       transform: translateY(-.2rem);
     }
   }
-`
+`;
 
 const About = styled.div`
-  padding: 8rem 2rem;
-
+  padding: 8rem 20rem;
+  line-height: 2rem;
   .header {
     font-size: 2rem;
     line-height: 2.5rem;
     text-align: center;
     margin-bottom: 2rem;
   }
-`
+  .column {
+    max-width: 1500px;
+    margin: 0 auto 0 auto;
+    line-height: 1.65;
+    padding: 1rem 1rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .columnLeft {
+    font-size: 1rem;
+    padding: 2rem 2rem;
+    background: url(/static/img/aboutPic.jpeg);
+    line-height: 2rem;
+    text-align: left;
+  }
+  .columnRight {
+    font-size: 1rem;
+    padding: 1rem 2rem;
+    line-height: 2rem;
+    text-align: left;
+  }
+`;
 
 const Events = styled.div`
   position: relative;
-  padding: 8rem 2rem;
+  padding: 8rem 20rem;
+  line-height: 2rem;
   background: url(/static/img/wine.jpg);
   background-position: center;
   background-size: cover;
@@ -123,7 +146,93 @@ const Events = styled.div`
     text-align: center;
     margin-bottom: 2rem;
   }
-`
+`;
+const Rates = styled.div`
+  padding: 8rem 2rem;
+  text-align: center;
+
+  .header {
+    font-size: 2rem;
+    line-height: 2.5rem;
+    text-align: center;
+    margin-bottom: 2rem;
+  }
+
+  .subHeader {
+    font-size: 1.5rem;
+  }
+  .column {
+    max-width: 1500px;
+    margin: 0 auto 0 auto;
+    line-height: 1.65;
+    padding: 1rem 1rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .columnLeft {
+    font-size: 1rem;
+    padding: 2rem 2rem;
+    border-right: 1px solid black;
+    line-height: 2rem;
+    text-align: left;
+  }
+  .columnRight {
+    font-size: 1rem;
+    padding: 1rem 2rem;
+    line-height: 2rem;
+    text-align: left;
+  }
+  .info {
+    font-size: 1rem;
+    font-style: italic;
+  }
+`;
+const Contact = styled.div`
+  padding: 1rem 2rem;
+  background: rgba(19, 18, 15, 0.75);
+  background-position: center;
+  background-size: cover;
+  color: white;
+  text-align: center;
+
+  .header {
+    font-size: 1.5rem;
+    text-align: center;
+    margin-bottom: .5rem;
+    text-shadow: 0 0 .05rem black, 0 0 .05rem black;
+  }
+
+  .column {
+    max-width: 1500px;
+    margin: 0 auto 0 auto;
+    line-height: 1.65;
+    padding: 1rem 1rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-shadow: 0 0 .08rem black, 0 0 .08rem black;
+  }
+  .columnLeft {
+    width: 15rem;
+    font-size: 1rem;
+    padding: .5rem 5rem;
+    margin-left: -9rem;
+    line-height: 2rem;
+  }
+  .columnRight {
+    font-size: 1rem;
+    padding: 1rem .5rem;
+    line-height: 2rem;
+  }
+  .contactBody {
+    font-size: 1rem;
+    font-style: italic;
+  }
+  .contactInfo {
+    font-size: 1.1rem;
+  }
+`;
 
 export default class Home extends Component {
   render() {
@@ -132,24 +241,38 @@ export default class Home extends Component {
         <Header>
           <div className="background" />
           <div className="content">
-            <Logo>City View Memorium</Logo>
-            <Navbar>
-              <ul>
-                <li>
-                  <a href="#about">About</a>
-                </li>
-                <li>
-                  <a href="#events">Events</a>
-                </li>
-                <li>
-                  <a href="#rates">Rates</a>
-                </li>
-                <li>
-                  <a href="#contact">Contact</a>
-                </li>
-              </ul>
-            </Navbar>
-            <div className="byline">Ogden's premiere event center</div>
+            <Logo>
+              City View
+              <br />
+              <br />
+              <br />
+              Memoriam
+              <Navbar>
+                <ul>
+                  <li>
+                    <a href="#about">About</a>
+                  </li>
+                  <li>
+                    <a href="#events">Events</a>
+                  </li>
+                  <li>
+                    <a href="#rates">Rates</a>
+                  </li>
+                  <li>
+                    <a href="#contact">Contact</a>
+                  </li>
+                </ul>
+              </Navbar>
+            </Logo>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <div className="byline">Salt Lake City's premiere event center</div>
           </div>
         </Header>
         <About id="about">
@@ -182,44 +305,68 @@ export default class Home extends Component {
           <div className="background" />
         </Events>
 
-        <div id="rates">
-          Rates -
-          <br />Reception
-          <br />$995 – Includes:
-          <br />Use of the building for 5 hours
-          <br />Tables and chairs for 80, setup
-          <br />Linens
-          <br />One staff member for assistance during event
-          <br />$100 of that $995 potentially refundable cleaning deposit
-          <br />$100 – additional hourly charge
+        <Rates id="rates">
+          <div className="header">Rates</div>
           <br />
-          <br />Luncheon/memorial service/family gathering
-          <br />$495 – Includes:
-          <br />Use of the building for 4 hours
-          <br />Tables and chairs for 80, setup
-          <br />Linens
-          <br />One staff member for assistance during event
-          <br />$100 of that $495 potentially refundable cleaning deposit
-          <br />$100 – additional hourly charge
-        </div>
-
-        <div id="contact">
-          Contact - Give us a call for more information about our building and
-          services. We are happy to assist you in planning your upcoming event.
-          <div>City View Memoriam</div>
-          <div>1001 East 11th Avenue</div>
-          <div>Salt Lake City, UT 84103</div>
-          <br />
-          <br />
-          <div>
-            <a href="tel:+18017466913">(801) 476 - 6913</a>
+          <div className="column">
+            <div className="columnLeft">
+              <div className="subHeader">
+                <strong>Reception</strong>
+              </div>
+              <br />$995 – Includes:
+              <br />
+              <br />Use of the building for 5 hours
+              <br />Tables and chairs for 80, setup
+              <br />Linens
+              <br />One staff member for assistance during event
+              <div className="info">
+                <br />$100 of that $995 potentially refundable cleaning deposit
+                <br />$100 – additional hourly charge
+              </div>
+            </div>
+            <br />
+            <br />
+            <div className="columnRight">
+              <div className="subHeader">
+                <strong>Luncheon, memorial service, or family gathering</strong>
+              </div>
+              <br />$495 – Includes:
+              <br />
+              <br />Use of the building for 4 hours
+              <br />Tables and chairs for 80, setup
+              <br />Linens
+              <br />One staff member for assistance during event
+              <div className="info">
+                <br />$100 of that $495 potentially refundable cleaning deposit
+                <br />$100 – additional hourly charge
+              </div>
+            </div>
           </div>
+        </Rates>
+
+        <Contact id="contact">
+          <div className="header">Contact Us</div>
           <br />
-          <br />
-          <br />
-          <br />
-        </div>
+          <div className="column">
+            <div className="columnLeft">
+              <div className="contactBody">
+                Give us a call for more information about our building and
+                services. We are happy to assist you in planning your upcoming
+                event.
+              </div>
+            </div>
+            <br />
+            <div className="columnRight">
+              <div className="contactInfo">
+                <div>City View Memoriam</div>
+                <div>1001 East 11th Avenue</div>
+                <div>Salt Lake City, UT 84103</div>
+                <a href="tel:+18017466913">(801) 476 - 6913</a>
+              </div>
+            </div>
+          </div>
+        </Contact>
       </div>
-    )
+    );
   }
 }
