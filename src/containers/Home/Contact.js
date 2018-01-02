65f27b252b5b872fa35c8f7e7662ff1b59187aeb
Tanner Linsley <tannerlinsley@gmail.com>
2018-01-02 16:13:25 -0700

    Contact form

--- src/containers/Home/Contact.js
+++ src/containers/Home/Contact.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Form, Text, TextArea } from 'react-form'
+import axios from 'axios'
 
 const Contact = styled.div`
   padding: 5rem 2rem;
@@ -16,6 +18,53 @@ const Contact = styled.div`
     text-shadow: 0 0 0.05rem black, 0 0 0.05rem black;
   }
 
+  form {
+    margin: 2rem 0 1rem;
+    > div {
+      width: 400px;
+      max-width: 100%;
+      margin: 0 auto;
+      margin-bottom: 1rem;
+      > div {
+        display: block;
+        margin-bottom: 0.2rem;
+        font-size: 0.9rem;
+        text-align: left;
+        opacity: 0.6;
+      }
+      input {
+        display: block;
+      }
+    }
+    input,
+    textarea {
+      border: 0;
+      background: rgba(0, 0, 0, 0.3);
+      color: white;
+      padding: 0.7rem;
+      width: 100%;
+      font-size: 1rem;
+      border-radius: 0.3rem;
+    }
+    button {
+      border: 0;
+      border-radius: 0.3rem;
+      background: rgb(198, 79, 104);
+      color: white;
+      appearance: none;
+      padding: 0.5rem;
+      font-size: 1rem;
+      cursor: pointer;
+      transition: all 0.1s ease-out;
+
+      :hover {
+        transform: scale(1.2);
+        background: rgb(191, 44, 75);
+        box-shadow: 0 15px 40px rgba(0, 0, 0, 0.2);
+      }
+    }
+  }
+
   .columns {
     display: flex;
     align-items: center;
@@ -24,9 +73,9 @@ const Contact = styled.div`
     text-shadow: 0 0 0.08rem black, 0 0 0.08rem black;
   }
   .column {
-    flex: 1 1 300px;
+    flex: 1 1 500px;
     padding: 1rem;
-    max-width: 15rem;
+    max-width: 20rem;
     font-size: 1rem;
     line-height: 2rem;
   }
@@ -41,25 +90,65 @@ const Contact = styled.div`
   }
 `
 
-export default () => (
-  <Contact id="contact">
-    <div className="header">Contact Us</div>
-    <div className="columns">
-      <div className="column">
-        Give us a call for more information about our building and services. We are happy to assist
-        you in planning your upcoming event.
-      </div>
-      <div className="column">
-        <div>City View Receptions</div>
-        <div>1001 East 11th Avenue</div>
-        <div>Salt Lake City, UT 84103</div>
-        <a href="tel:+18017466913" className="phone">
-          (801) 476 - 6913
-        </a>
-      </div>
-    </div>
-    <div className="copyright">
-      Copyright &copy; {new Date().getYear() + 1900} City View Memorium
-    </div>
-  </Contact>
-)
+const encode = data =>
+  Object.keys(data)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
+    .join('&')
+
+export default class extends React.Component {
+  render () {
+    return (
+      <Contact id="contact">
+        <div className="header">Contact Us</div>
+        <Form
+          onSubmit={values => {
+            axios.post('/', encode({ 'form-name': 'contact', ...values }), {
+              headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            })
+          }}
+        >
+          {({ handleSubmit }) => (
+            <form onSubmit={handleSubmit}>
+              <div>
+                <div>Name</div>
+                <Text field="name" placeholder="John Doe" />
+              </div>
+              <div>
+                <div>Email</div>
+                <Text field="email" placeholder="johndoe@gmail.com" />
+              </div>
+              <div>
+                <div>Phone</div>
+                <Text field="phone" placeholder="(555) 555-555" />
+              </div>
+              <div>
+                <div>Message</div>
+                <TextArea field="message" rows="5" placeholder="Your message to us :)" />
+              </div>
+              <div>
+                <button type="submit">Submit</button>
+              </div>
+            </form>
+          )}
+        </Form>
+        <div className="columns">
+          <div className="column">
+            Or give us a call for more information about our building and services. We are happy to
+            assist you in planning your upcoming event.
+          </div>
+          <div className="column">
+            <div>City View Receptions</div>
+            <div>1001 East 11th Avenue</div>
+            <div>Salt Lake City, UT 84103</div>
+            <a href="tel:+18017466913" className="phone">
+              (801) 476 - 6913
+            </a>
+          </div>
+        </div>
+        <div className="copyright">
+          Copyright &copy; {new Date().getYear() + 1900} City View Memorium
+        </div>
+      </Contact>
+    )
+  }
+}
